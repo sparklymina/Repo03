7c08765fb08f2589a7e01e5aeffded52b90b5556
sparklymina <ecausevic93@gmail.com>
2021-09-15 15:16:25 -0400

    adding ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,16 @@
+function main() {
+    // Only change code below this line
+    //MOOSES
+    /* M
+    O
+    O
+    S
+    E
+    S*/
+    // Only change code above this line
+
+}
+
+console.log(main());
+
+module.exports = main;
